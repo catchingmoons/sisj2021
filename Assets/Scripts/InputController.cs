ef4e0cda1589795543e6ac892db0e2bc40f5bca2
MoonCatcher <catchingmoonsgames@gmail.com>
2021-11-06 11:54:58 -0700

    Added basic music player. P,L,N to control.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,12 +6,22 @@ public class InputController : MonoBehaviour
 {
     [SerializeField]
     private PlayerController player;
+    [SerializeField]
+    private MusicController music;
 
     private Vector2 direction;
 
     public void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //DEBUGGING - probably don't want keys to control music
+        if (music.isActiveAndEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.L)) music.StopMusic();
+            if (Input.GetKeyDown(KeyCode.P)) music.PlayMusic();
+            if (Input.GetKeyDown(KeyCode.N)) music.NextTrack();
+        }
     }
 
     public void FixedUpdate()
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicController : MonoBehaviour
+{
+    [SerializeField]
+    private string directory;
+
+    private AudioSource source;
+    private int currentClipNdx;
+
+    private Object[] clips;
+
+    public void Awake()
+    {
+        source = GetComponent<AudioSource>();
+
+        clips = Resources.LoadAll(directory, typeof(AudioClip));
+
+        if (clips.Length > 0)
+        {
+            source.clip = (AudioClip)clips[0];
+            PlayMusic();
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+    }
+
+    public void NextTrack()
+    {
+        StopMusic();
+        currentClipNdx = (currentClipNdx + 1) % clips.Length;
+        source.clip = (AudioClip)clips[currentClipNdx];
+        PlayMusic();
+    }
+}
