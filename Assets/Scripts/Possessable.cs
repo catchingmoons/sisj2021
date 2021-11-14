3411fa90528ac95a746d88226bcc23a71096435d
Seth Paxton <spaxton0492@gmail.com>
2021-11-13 21:33:41 -0800

    Assets, Shaders, and more!
    Adding new FBX assets from Andres (now with proper material groups), added a toon shader and a couple example materials that use it, added an outline package from the Unity Asset store and hooked it up to the possessable object, switched the camera to orthographic for that isometric feel

--- Assets/Scripts/Possessable.cs
+++ Assets/Scripts/Possessable.cs
@@ -6,5 +6,30 @@ public class Possessable : MonoBehaviour
 {
     [SerializeField]
     public bool Unlocked;
+    private Outline Outliner;
+
+    void Start()
+    {
+        Outliner = GetComponent<Outline>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && Unlocked)
+        {
+            Outliner.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Outliner.enabled = false;
+        }
+        
+    }
 }
+
+
 
